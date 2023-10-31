d963a56a2d58887acc3c1572883c2d6b8bb2e995
Ion <ion.boleac2@gmail.com>
2023-10-31 17:37:24 +0100

    #

---
+++ utility_functions.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy import spatial
+import random
+
+
+def generate_random_network_tree(n_nodes: int, k_flow: int, edge_dim: int) -> nx.Graph:
+    """
+    Function to generate a random network tree with k flow
+    :param 
+        n_nodes: number of nodes in the tree
+        k_flow: number of flow in the tree
+        edge_dim: dimension of the edge of the square
+    :return
+        Tree: random network tree with k flow
+    """
+    Tree = nx.binomial_tree(n_nodes)
+    for (u, v) in Tree.edges():
+        Tree.edges[u, v]['weight'] = random.randint(1, 10)
+    return Tree
+
+def draw_tree(Tree : nx.Graph):
+    """
+    Function to draw a tree using networkx
+    :param 
+        Tree: Tree to be drawn
+    """
+    colors = {'colonnina':'green', 'no_colonnina':'grey', 'o_k':'red', 'd_k':'yellow'}
+    #pos = nx.spring_layout(Tree)
+    nx.draw(Tree, with_labels=True)
+    plt.show()
+
+
+def get_distance(point1, point2):
+    """
+    Function to get the distance between two points
+    :param
+        point1: first point
+        point2: second point
+    :return
+        distance between the two points
+    """
+    return spatial.distance.euclidean(point1, point2)
